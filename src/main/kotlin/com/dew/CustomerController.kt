e99f841274c5b5c76202d57721df49f24fe2c928
Carlos Pavajeau <cantte098@gmail.com>
2022-04-21 09:41:43 -0500

    feat: add jwt authentication
    Closes #2

--- src/main/kotlin/com/dew/CustomerController.kt
+++ src/main/kotlin/com/dew/CustomerController.kt
@@ -8,11 +8,14 @@ import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule
 import reactor.core.publisher.Mono
 import javax.validation.Valid
 import javax.validation.constraints.NotBlank
 
 @Controller("/customers")
+@Secured(SecurityRule.IS_AUTHENTICATED)
 open class CustomerController(private val customerService: CustomerService) {
 
     @Post
