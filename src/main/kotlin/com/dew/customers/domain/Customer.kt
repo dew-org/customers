bb2da870d977781f6f032cb34e3d0fedaf57de5b
Carlos Pavajeau <cantte098@gmail.com>
2022-05-09 16:24:22 -0500

    fix: remove use of pojo codec
    When generating and executing an image with GraalVM, with the default configurations and using the codec for the POJOS, the application fails with the exception `CodecConfigurationException: Can't find a codec for class [...]`.
    
    This error is solved by doing a manual mapping of the attributes.

--- src/main/kotlin/com/dew/customers/domain/Customer.kt
+++ src/main/kotlin/com/dew/customers/domain/Customer.kt
@@ -11,16 +11,31 @@ import java.util.Date
 
 @Introspected
 data class Customer @Creator @BsonCreator constructor(
-    @field:BsonProperty("_id") @param:BsonProperty("_id") @field:BsonId val id: String,
-    @field:BsonProperty("name") @param:BsonProperty("name") val name: String,
-    @field:BsonProperty("lastName") @param:BsonProperty("lastName") val lastName: String,
-    @field:BsonProperty("phoneNumber") @param:BsonProperty("phoneNumber") var phoneNumber: String?,
-    @field:BsonProperty("email") @param:BsonProperty("email") var email: String?,
-) {
-    constructor(id: String, name: String, lastName: String) : this(id, name, lastName, null, null)
+    @field:BsonProperty("_id")
+    @param:BsonProperty("_id")
+    @field:BsonId val
+    id: String,
+
+    @field:BsonProperty("name")
+    @param:BsonProperty("name")
+    val name: String,
+
+    @field:BsonProperty("lastName")
+    @param:BsonProperty("lastName")
+    val lastName: String,
+
+    @field:BsonProperty("phoneNumber")
+    @param:BsonProperty("phoneNumber")
+    var phoneNumber: String?,
+
+    @field:BsonProperty("email")
+    @param:BsonProperty("email")
+    var email: String?,
 
     @field:BsonProperty("createdAt")
     val createdAt: Date = Date.from(Instant.now(Clock.systemUTC()))
+) {
+    constructor(id: String, name: String, lastName: String) : this(id, name, lastName, null, null)
 
     @field:BsonProperty("updatedAt")
     var updatedAt: Date? = null
--- src/main/kotlin/com/dew/customers/infrastructure/persistence/mongo/MongoDbCustomerRepository.kt
+++ src/main/kotlin/com/dew/customers/infrastructure/persistence/mongo/MongoDbCustomerRepository.kt
@@ -6,21 +6,46 @@ import com.mongodb.client.model.Filters
 import com.mongodb.reactivestreams.client.MongoClient
 import com.mongodb.reactivestreams.client.MongoCollection
 import jakarta.inject.Singleton
+import org.bson.Document
 import reactor.core.publisher.Mono
 
 @Singleton
 class MongoDbCustomerRepository(
-    private val mongoDbConfiguration: MongoDbConfiguration, private val mongoClient: MongoClient
+    private val mongoDbConfiguration: MongoDbConfiguration,
+    private val mongoClient: MongoClient
 ) : CustomerRepository {
 
-    override fun save(customer: Customer): Mono<Boolean> =
-        Mono.from(collection.insertOne(customer)).map { true }.onErrorReturn(false)
+    override fun save(customer: Customer): Mono<Boolean> {
+        val document = Document("_id", customer.id)
+            .append("name", customer.name)
+            .append("lastName", customer.lastName)
+            .append("phoneNumber", customer.phoneNumber)
+            .append("email", customer.email)
+            .append("createdAt", customer.createdAt)
+
+        return Mono.from(collection.insertOne(document)).map { true }
+            .onErrorReturn(false)
+    }
 
     override fun findById(id: String): Mono<Customer> {
         return Mono.from(collection.find(Filters.eq("_id", id)))
+            .mapNotNull { document ->
+                val customer = Customer(
+                    id = document["_id"].toString(),
+                    name = document.getString("name"),
+                    lastName = document.getString("lastName"),
+                    phoneNumber = document.getString("phoneNumber"),
+                    email = document.getString("email"),
+                    createdAt = document.getDate("createdAt")
+                )
+
+                customer.updatedAt = document.getDate("updatedAt")
+
+                customer
+            }
     }
 
-    private val collection: MongoCollection<Customer>
+    private val collection: MongoCollection<Document>
         get() = mongoClient.getDatabase(mongoDbConfiguration.name)
-            .getCollection(mongoDbConfiguration.collection, Customer::class.java)
+            .getCollection(mongoDbConfiguration.collection)
 }
